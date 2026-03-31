cbbd098eb800169fe9c6f582f0ff3dd539cfc5c4
Aayush Raj <aayush.ardy@gmail.com>
2026-03-31 21:39:04 +0530

    feat(db): export realtimeDb and realtimePool from db entry point

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,3 +2,6 @@ export * from "./config/index.js";
 export * as schema from "./schema/index.js";
 export * as webSchema from "./schema/web.js";
 export * as realtimeSchema from "./schema/realtime.js";
+
+// Export realtime db instance for easy import
+export { db as realtimeDb, pool as realtimePool } from "./realtime.js";
